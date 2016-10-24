24751d3197d2f50af3c8598dfbb5b12a036a7a54
Besart Hoxhaj <besartshyti@gmail.com>
2016-10-24 11:21:21 +0100

    add simple sync version for tape

---
+++ tape/sync.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var test = function(description,cb) {
+  var t = {
+    equal: function (actual,expected,mess) {
+      if(actual === expected) {
+        console.log('PASS: ' + mess);
+      } else {
+        console.error('ERROR: ' + mess);
+      }
+    }
+  };
+  cb(t);
+}
+
+test('do something', function (t) {
+  t.equal('one','one','ok!');
+});
+
+test('do something wrong', function (t) {
+  t.equal('one','two','not!');
+});
