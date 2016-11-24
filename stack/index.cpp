2e8659358b58555c3f8133769aa636161dacadd4
besart <besartshyti@gmail.com>
2016-11-24 17:30:23 +0000

    simple implementation in c++

--- stack/index.cpp
+++ stack/index.cpp
@@ -3,17 +3,31 @@
 class Stack {
 private:
   int top;
+  int A[10];
 public:
   Stack() {
     top = -1;
   }
+  void push(int num) {
+    A[++top] = num;
+  }
+  void pop() {
+    top--;
+  }
   void print() {
-    std::cout << "Hello, Stack!" << '\n';
+    for(int ii = 0; ii <= top; ii++) {
+      std::cout << A[ii] << '\n';
+    }
   }
 };
 
 int main() {
   Stack simpleStack;
+  simpleStack.push(2);
+  simpleStack.push(1);
+  simpleStack.push(4);
+  simpleStack.push(3);
+  simpleStack.pop();
   simpleStack.print();
   return 0;
 }
