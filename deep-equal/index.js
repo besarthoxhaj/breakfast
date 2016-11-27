b91236f79c8768ed5f676f62b8dd3f7a0c35a71b
besart <besartshyti@gmail.com>
2016-11-27 14:31:44 +0000

    improve testing framework

--- deep-equal/index.js
+++ deep-equal/index.js
@@ -2,15 +2,24 @@
 /* @flow */
 
 var deepEqual = function(x, y) {
-  if(typeof x !== 'object' && typeof y !== 'object') {
-    return x === y;
-  } else if (Object.keys(x).length !== Object.keys(y).length) {
-    return false;
-  } else {
-    return Object.keys(x).every(function(key) {
-      return deepEqual(x[key], y[key]);
-    });
+  var result = (function testEquality (a,b){
+    if(typeof a !== 'object' && typeof b !== 'object') {
+      return a === b;
+    } else if (Object.keys(a).length !== Object.keys(b).length) {
+      return false;
+    } else {
+      return Object.keys(a).every(function(key) {
+        return testEquality(a[key], b[key]);
+      });
+    }
+  }(x,y));
+  if(result === false) {
+    console.log(`
+    ACTUAL: ${JSON.stringify(x)}
+    EXPECTED: ${JSON.stringify(y)}
+    `);
   }
+  return result;
 };
 
 if (typeof module === 'object' && module.exports) {
--- tape/index.js
+++ tape/index.js
@@ -2,35 +2,44 @@ var test = (function () {
   'use strict';
 
   var callbacks = [];
+  var testResult = {pass:0,fail:0};
 
   function test(description, cb) {
     var t = {
       equal: function(actual, expected, message) {
         if (actual === expected) {
           console.log('PASS: ' + message);
+          testResult.pass++;
         } else {
           console.error('ERROR: ' + message + '. Expected: ' + expected + '. Actual: ' + actual);
+          testResult.fail++;
         }
       },
       notEqual: function(actual, expected, message) {
         if (actual !== expected) {
           console.log('PASS: ' + message);
+          testResult.pass++;
         } else {
           console.error('ERROR: ' + message + '. Expected: ' + expected + '. Actual: ' + actual);
+          testResult.fail++;
         }
       },
       ok: function(value, message) {
         if (value) {
           console.log('PASS: ' + message);
+          testResult.pass++;
         } else {
           console.error('ERROR: ' + message);
+          testResult.fail++;
         }
       },
       notOk: function(value, message) {
         if (!value) {
           console.log('PASS: ' + message);
+          testResult.pass++;
         } else {
           console.error('ERROR: ' + message);
+          testResult.fail++;
         }
       },
       end: function() {
@@ -41,7 +50,11 @@ var test = (function () {
           console.log('#',nextTest.mess);
           nextTest.fun();
         } else {
-          console.log('TEST COMPLETED');
+          console.log(`
+          TEST COMPLETED:
+          PASS: ${testResult.pass}
+          FAIL: ${testResult.fail}
+          `);
         }
       }
     };
