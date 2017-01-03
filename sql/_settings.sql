9c3d8e4ee464e50e0aa5deb4c230a240fa78da89
besart <besartshyti@gmail.com>
2017-01-03 22:49:08 +0000

    wip

--- sql/_settings.sql
+++ sql/_settings.sql
@@ -1,6 +1,8 @@
 /**
- * Settings
+ * Settings.
+ * 1) will disable (or enable) the pretty mode. The pretty mode is useful
+ * when dealing with one big row, not that useful when dealing with many
+ * short rows.
  */
-
 -- \x on;
 \x off;
